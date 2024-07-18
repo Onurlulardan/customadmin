626558e3082d8e047f410a346a6a06c8ba45d71a
Onurlulardan <altuntasonur@outlook.com.tr>
2024-07-18 17:23:36 +0300

    chore: Update DataTable component to include fake user data

--- src/admin/pages/dashboard/index.jsx
+++ src/admin/pages/dashboard/index.jsx
@@ -1,23 +1,10 @@
 import React, { useState } from "react";
 import { Box, VStack, Heading } from "@chakra-ui/react";
 import DataTable from "../../../components/datatable";
+import { data } from "../../../data/fakeUserData";
 
 const Dashboard = () => {
   // Örnek veri ve sütunlar
-  const data = [
-    { id: 1, name: "John Doe", age: 28, email: "john@example.com" },
-    { id: 2, name: "Jane Smith", age: 34, email: "jane@example.com" },
-    { id: 3, name: "Alice Johnson", age: 22, email: "alice@example.com" },
-    { id: 4, name: "Bob Brown", age: 45, email: "bob@example.com" },
-    { id: 5, name: "Charlie Davis", age: 30, email: "charlie@example.com" },
-    { id: 6, name: "David Evans", age: 29, email: "david@example.com" },
-    { id: 7, name: "Ella Green", age: 25, email: "ella@example.com" },
-    { id: 8, name: "Frank Harris", age: 50, email: "frank@example.com" },
-    { id: 9, name: "Grace Ivers", age: 27, email: "grace@example.com" },
-    { id: 10, name: "Hank James", age: 33, email: "hank@example.com" },
-    { id: 11, name: "Ivy King", age: 21, email: "ivy@example.com" },
-    { id: 12, name: "Jack Lewis", age: 40, email: "jack@example.com" },
-  ];
 
   const columns = [
     { key: "id", header: "ID" },
