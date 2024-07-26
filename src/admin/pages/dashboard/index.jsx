73890c2c07030372f5c905b389b707ace48c7ec5
Onurlulardan <altuntasonur@outlook.com.tr>
2024-07-26 09:44:16 +0300

    refactor: Update DataTable component to include defaultAddButton prop

--- src/admin/pages/dashboard/index.jsx
+++ src/admin/pages/dashboard/index.jsx
@@ -29,10 +29,25 @@ const columns = [
     ),
     width: "80px",
     type: "File",
+    acceptedFileTypes: "image/*",
+    valueType: "base64",
   },
-  { key: "name", header: "Ad", type: "String" },
-  { key: "age", header: "Yaş", type: "Number" },
-  { key: "email", header: "Email", type: "String" },
+  {
+    key: "name",
+    header: "Ad",
+    type: "String",
+    isRequired: true,
+    maxLength: 50,
+  },
+  {
+    key: "age",
+    header: "Yaş",
+    type: "Number",
+    isRequired: true,
+    min: 0,
+    max: 120,
+  },
+  { key: "email", header: "Email", type: "String", isRequired: true },
 ];
 
 //ContexMenu itemleri
